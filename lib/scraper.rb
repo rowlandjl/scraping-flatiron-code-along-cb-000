5741c413ac8b5c152a473809ea554c1779c1dec9
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 20:14:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,13 +7,6 @@ class Scraper
   
   def get_page 
     Nokorgiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    
-    doc.css(".post").each do |post|
-      course = Course.new 
-      course.title = post.css("h2").text 
-      course.schedule = post.css(".date").text 
-      course.description = post.css("p").text 
-    end 
   end 
   
     def print_courses
